214862abffc991e803d5dd01d4a9f5b70d19c4b0
Anton Yulbarysau <io57196657@gmail.com>
2020-06-07 20:32:48 +0300

    complete

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-var money = 1200;
-var income = 'макдоналдс';
-let addExpenses = 'Интернет, Такси, Коммуналка, Еда';
-let deposit = true;
-const mission = 2345;
-const period = 3;
+let num = 266219;
+let mul = 1;
+let pow;
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-
-console.log(addExpenses.length);
-
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + (mission) + ' рублей/долларов/гривен/юани');
-
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-console.log(addExpenses.toLowerCase(addExpenses.split(', ')));
-
-let budgetDay = money / 30;
-
-console.log(budgetDay);
+for (let i = 0; i < num.toString().length; i++) {
+    mul = mul * num.toString()[i];
+}
+ 
+console.log(mul);
+pow = mul ** 3;
+console.log(pow.toString().substr(0, 2));
