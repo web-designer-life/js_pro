4f5e3135a884d561fb62395542f0cdd4779f402e
Anton Yulbarysau <io57196657@gmail.com>
2020-06-12 21:34:26 +0300

    lesson03

--- main.js
+++ main.js
@@ -1,11 +1,60 @@
-let num = 266219;
-let mul = 1;
-let pow;
+let language;
+let lang;
 
-for (let i = 0; i < num.toString().length; i++) {
-    mul = mul * num.toString()[i];
+var array = [];
+array['ru'] = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+array['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+language = confirm('Язык английский?');
+
+if (language) {
+    lang = 'en';
+} else {
+    lang = 'ru';
 }
- 
-console.log(mul);
-pow = mul ** 3;
-console.log(pow.toString().substr(0, 2));
+
+if (lang == 'en') {
+    console.log('Monday');
+    console.log('Tuesday');
+    console.log('Wednesday');
+    console.log('Thursday');
+    console.log('Friday');
+    console.log('Saturday');
+    console.log('Sunday');
+} else {
+    console.log('Понедельник');
+    console.log('Вторник');
+    console.log('Среда');
+    console.log('Четверг');
+    console.log('Пятница');
+    console.log('Суббота');
+    console.log('Воскресенье');
+}
+
+console.log('');
+
+switch (lang) {
+    case 'en':
+        console.log('Monday');
+        console.log('Tuesday');
+        console.log('Wednesday');
+        console.log('Thursday');
+        console.log('Friday');
+        console.log('Saturday');
+        console.log('Sunday');
+        break;
+    case 'ru':
+        console.log('Понедельник');
+        console.log('Вторник');
+        console.log('Среда');
+        console.log('Четверг');
+        console.log('Пятница');
+        console.log('Суббота');
+        console.log('Воскресенье');
+        break;
+}
+
+console.log('');
+
+console.log(array[lang]);
+
